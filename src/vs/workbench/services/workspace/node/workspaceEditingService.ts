65a05c01048fa80c02cf07f056f58c902fc03455
Sandeep Somavarapu <sasomava@microsoft.com>
2017-07-12 00:38:34 +0200

    Transition from 1 to n folders - Create a new workspace with folders - Open the workspace in the current window

--- src/vs/workbench/services/workspace/node/workspaceEditingService.ts
+++ src/vs/workbench/services/workspace/node/workspaceEditingService.ts
@@ -8,9 +8,13 @@
 import { IWorkspaceEditingService } from 'vs/workbench/services/workspace/common/workspaceEditing';
 import URI from 'vs/base/common/uri';
 import { equals, distinct } from 'vs/base/common/arrays';
+import * as uuid from 'vs/base/common/uuid';
+import * as path from 'path';
 import { TPromise } from "vs/base/common/winjs.base";
 import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
+import { IWindowsService } from 'vs/platform/windows/common/windows';
 import { IEnvironmentService } from 'vs/platform/environment/common/environment';
+import { IChoiceService, Severity } from 'vs/platform/message/common/message';
 import { IJSONEditingService } from 'vs/workbench/services/configuration/common/jsonEditing';
 
 export class WorkspaceEditingService implements IWorkspaceEditingService {
@@ -20,14 +24,15 @@ export class WorkspaceEditingService implements IWorkspaceEditingService {
 	constructor(
 		@IJSONEditingService private jsonEditingService: IJSONEditingService,
 		@IWorkspaceContextService private contextService: IWorkspaceContextService,
-		@IEnvironmentService private environmentService: IEnvironmentService
+		@IEnvironmentService private environmentService: IEnvironmentService,
+		@IChoiceService private choiceService: IChoiceService,
+		@IWindowsService private windowsService: IWindowsService
 	) {
 	}
 
 	private supported(): boolean {
-		const workspace = this.contextService.getWorkspace();
-		if (!workspace && !workspace.configuration) {
-			return false; // we need a workspace with configuration to begin with
+		if (this.contextService.hasWorkspace()) {
+			return false; // we need a workspace to begin with
 		}
 
 		// TODO@Ben multi root
@@ -67,6 +72,9 @@ export class WorkspaceEditingService implements IWorkspaceEditingService {
 
 		// Apply to config
 		if (newWorkspaceRoots.length) {
+			if (!workspace.configuration) {
+				return this.createWorkspace(newWorkspaceRoots);
+			}
 			return this.jsonEditingService.write(workspace.configuration, { key: 'folders', value: newWorkspaceRoots }, true);
 		} else {
 			// TODO: Sandeep - Removing all roots?
@@ -83,5 +91,17 @@ export class WorkspaceEditingService implements IWorkspaceEditingService {
 		// Prevent duplicates
 		const validatedRoots = distinct(roots.map(root => root.toString(true /* skip encoding */)));
 		return validatedRoots;
+	}
+
+	private createWorkspace(newRoots: string[]): TPromise<void> {
+		return this.choiceService.choose(Severity.Info, 'This action will create a new workspace. Would you like to copy settings from current workspace?', ['Yes', 'No'], 1)
+			.then(option => this._doCreateWorkspace(newRoots))
+			.then(newConfiguration => this.windowsService.openWindow([newConfiguration]));
+	}
+
+	private _doCreateWorkspace(folders: string[]): TPromise<string> {
+		const ID = uuid.generateUuid();
+		const newConfiguration = path.join(this.environmentService.appSettingsHome, `${ID}.vscode`);
+		return this.jsonEditingService.write(URI.file(newConfiguration), { key: '', value: { ID, folders } }, true).then(() => newConfiguration);
 	}
 }
